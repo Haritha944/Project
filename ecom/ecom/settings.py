559ab73e803fd21c14071c1ad1e138c5cdc97ac4
Haritha944 <harithams23@gmail.com>
2024-01-07 15:43:54 +0530

    Revert "Revert "Revert "Resolve conflicts with Projectfirst"""
    This reverts commit 6285bf637f25d0815a0e08786c8fb0e7d6da926d.

--- ecom/ecom/settings.py
+++ ecom/ecom/settings.py
@@ -48,10 +48,6 @@ INSTALLED_APPS = [
     'dashboard',
     'user',
     'cart',
-<<<<<<< HEAD
-    'order',
-=======
->>>>>>> 8b56765e6bed435d8dfc3a6abdb477c47957515e
     
 ]
 
@@ -163,8 +159,3 @@ AUTH_USER_MODEL = 'user.User'
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.PBKDF2PasswordHasher',
 ]
-<<<<<<< HEAD
-key_id = 'rzp_test_zLLrBmHDjYzLTa'
-key_secret = 'RZzrXnbKkKZyFzvIGk57In95'
-=======
->>>>>>> 8b56765e6bed435d8dfc3a6abdb477c47957515e
