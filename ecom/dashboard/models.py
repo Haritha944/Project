ab8a7f4db19e39bd6a281847b840246dd05e683b
Haritha944 <harithams23@gmail.com>
2024-02-05 11:22:53 +0530

    referral model added

--- ecom/dashboard/models.py
+++ ecom/dashboard/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from user.models import User
+from django.utils import timezone
 
 
+
+class Referral(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    referral_code = models.CharField(max_length=10,unique=True)
+    referred_by = models.ForeignKey(User,related_name='referrals',null=True,blank=True,on_delete=models.SET_NULL)
+    created_date = models.DateTimeField(default=timezone.now) 
+
+
+    def __str__(self):
+        return f"{self.user.name}'s Referral: {self.referral_code} Referred: {self.referred_by}"
+    
 
